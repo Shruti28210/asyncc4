78967d113f4975057539406d9d8a5b71adf457fb
Shruti Waghmode <waghmodeshru@gmail.com>
2022-05-02 17:03:36 +0530

    added

--- src/components/NewOrder.jsx
+++ src/components/NewOrder.jsx
@@ -1,4 +1,45 @@
+import { useState,useEffect } from "react";
+import axios from "axios";
+
 export const NewOrder = () => {
+
+   
+  const [data,setData] = useState([]);
+  const inisial_state={
+    
+    name:"",
+    new_problem:"",
+    brand:""
+  }
+  const [neworder,setNewOrder]=useState(inisial_state)
+  const {name,new_problem,brand} = neworder;
+  const handleChange  = (e) =>{
+    let{name,value} = e.target;
+
+    setNewOrder((prev) => ({
+       ...prev,[name] : value
+    }))
+}
+
+const handlesubmit = (e) =>{
+  e.preventDefault();
+ // console.log(studentdata)
+ axios.post("http://localhost:8080/orders",neworder)
+ setNewOrder(inisial_state)
+ fetchdata()
+
+}
+
+const fetchdata = () =>{
+  axios.get("http://localhost:8080/orders").then((res) => {
+      setData(res.data);
+  })
+}
+
+useEffect(() => {
+  fetchdata()
+},[])
+//console.log(neworder)
   // Get data of only this user. store it in redux
   // GET /orders?owner_name=john will give you all order of user john
   //  on submit click create a new order, new order has status `Not Accepted`
@@ -10,6 +51,7 @@ export const NewOrder = () => {
           type="text"
           name="problem"
           placeholder="Enter problem"
+          onChange={handleChange}
         />
         {/* This input is readonly, it's coming from redux */}
         <input
@@ -17,6 +59,7 @@ export const NewOrder = () => {
           type="text"
           name="owner_name"
           placeholder="yourname"
+          onChange={handleChange}
           readOnly
         />
         <input
@@ -24,9 +67,10 @@ export const NewOrder = () => {
           type="text"
           name="brand"
           placeholder="Enter brand name"
+          onChange={handleChange}
         />
         {/* Create new problem, show it in below form immediately */}
-        <button className="submit">submit</button>
+        <button className="submit" onClick={handlesubmit}>submit</button>
       </div>
 
       <div className="pastOrders">
